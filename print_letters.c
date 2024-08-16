9631ae2946bd9cda7b095efb9e8eb75c72a97d82
Flavio Avdulla <a.flavio4366@gmail.com>
2024-08-16 20:31:37 +0000

    changes

--- print_letters.c
+++ print_letters.c
@@ -3,14 +3,58 @@ void print_E()
 {
 	int i, j;
 
-	for (i = 0; i < 7; i++)
+	// for (i = 0; i < 7; i++)
+	// {
+	// 	if(i == 0 || i == 3 || i == 6)
+	// 	{
+	// 		for (j = 0; j < 6; j++)
+	// 		{
+	// 			printf(" *");
+	// 		}
+	// 	}
+	// 	printf(" *");
+	// 	printf("\n");
+	// }
+
+	// for(i = 0; i < 7; i++)
+	// {
+	// 	if(i == 0 || i == 6) //ROWS
+	// 	{
+	// 		for (j = 0; j < 6; j++) //COLUMNS
+	// 		{
+	// 			printf(" *");
+	// 		}
+	// 	}
+	// 	printf(" *");
+	// 	printf("\n");
+	// }
+
+	for(i = 0; i < 7; i++)
 	{
-		if(i == 0 || i == 3 || i == 6)
+		if(i == 1) //ROWS
 		{
-			for (j = 0; j < 6; j++)
+			for (j = 0; j < 1; j++) //COLUMNS
 			{
 				printf(" *");
 			}
+		}
+
+		if(i == 1) //ROWS
+		{
+			for (j = 0; j < 0; j++) //COLUMNS
+			{
+				printf(" *");
+			}
+		}
+
+		if(i == 2) //ROWS
+		{
+			for (j = 0 && j != 1; j < 2; j++) //COLUMNS
+			{
+				printf(" *");
+				
+			}
+			
 		}
 		printf(" *");
 		printf("\n");
