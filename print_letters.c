7434d3bce40b028d5ce7e409ac94bc1ef2ea991e
Flavio Avdulla <a.flavio4366@gmail.com>
2024-08-19 07:36:29 +0000

    changes

--- print_letters.c
+++ print_letters.c
@@ -3,72 +3,19 @@ void print_E()
 {
 	int i, j;
 
-	// for (i = 0; i < 7; i++)
-	// {
-	// 	if(i == 0 || i == 3 || i == 6)
-	// 	{
-	// 		for (j = 0; j < 6; j++)
-	// 		{
-	// 			printf(" *");
-	// 		}
-	// 	}
-	// 	printf(" *");
-	// 	printf("\n");
-	// }
-
-	// for(i = 0; i < 7; i++)
-	// {
-	// 	if(i == 0 || i == 6) //ROWS
-	// 	{
-	// 		for (j = 0; j < 6; j++) //COLUMNS
-	// 		{
-	// 			printf(" *");
-	// 		}
-	// 	}
-	// 	printf(" *");
-	// 	printf("\n");
-	// }
-
-	for(i = 0; i < 7; i++)
+	for (i = 0; i < 7; i++)
 	{
-		if(i == 1) //ROWS
+		if(i == 0 || i == 3 || i == 6)
 		{
-			for (j = 0; j < 1; j++) //COLUMNS
+			for (j = 0; j < 6; j++)
 			{
 				printf(" *");
 			}
 		}
-
-		if (i == 2) { // ROWS
-            for (int j = 0; j < 2; j++) // COLUMNS
-			{
-                if (j == 0 || j == 2)  // Skip printing when j == 1
-				{
-                    printf(" *");
-                }
-				else
-				{
-					printf(" ");
-				}
-            }
-
-		if (i == 3) { // ROWS
-            for (int j = 0; j < 2; j++) // COLUMNS
-			{
-                if (j == 0 || j == 2)  // Skip printing when j == 1
-				{
-                    printf(" *");
-                }
-				else
-				{
-					printf(" ");
-				}
-            }
-        }
 		printf(" *");
 		printf("\n");
 	}
-	
+
 
 }
 
