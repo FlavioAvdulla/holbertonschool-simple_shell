597fcc7f61682226f0eb1d281d8a3505d430cfbd
StilianSaka <stilisaka@gmail.com>
2024-08-23 15:06:27 +0000

    Making changes on the edge cases

--- environment_utils.c
+++ environment_utils.c
@@ -1,59 +1,87 @@
 #include "main.h"
+
 /**
- * _str_n_cmp - String compares also bytes
- * @s1: First operand a string
- * @s2: Second operand a string
- * @n: Third operand an int
- * Return: Returns 1, -1 or 0
- */
-int _str_n_cmp(char *s1, char *s2, int n)
-{
-	char c1;
-	char c2;
-
-	while (n--)
-	{
-		c1 = *s1++;
-		c2 = *s2++;
-
-		if (c1 == '\0' || c1 != c2)
-			return (c1 > c2 ? 1 : (c1 < c2 ? -1 : 0));
-	}
-
-	return (0);
-}
-/**
- * _printenv - Function that prints an environment
- * Return: Void
+ * _printenv - Prints the environment
+ * Return: 0 on success, -1 on failure
  */
 int _printenv(void)
 {
-	char *str = environ[0];
-	int i = 0;
+    int i = 0;
 
-	while (str[i] != '\0')
-	{
-		write(1, str, _strlen(str));
-		write(1, "\n", 1);
-		str = environ[i];
-		++i;
-	}
-	return (0);
+    if (environ == NULL)
+    {
+        fprintf(stderr, "Environment variables not available\n");
+        return (-1);
+    }
+
+    while (environ[i] != NULL)
+    {
+        printf("%s\n", environ[i]);
+        i++;
+    }
+    return (0);
 }
-/**
- * _getenv - imitation of getenv
- * @name: env name
- * Return: found name or NULL
- */
-char *_getenv(char *name)
-{
-	int len = _strlen(name);
-	int i;
 
-	for (i = 0; environ[i] != NULL; i++)
-	{
-		if (_str_n_cmp(environ[i], name, len) == 0)
-			return (&environ[i][len]);
-	}
-	return (NULL);
+/**
+ * _getenv - Gets the value of an environment variable
+ * @var: The environment variable to get
+ * Return: The value of the environment variable, or NULL if not found
+ */
+char *_getenv(char *var)
+{
+    int i, len_var;
+
+    if (var == NULL)
+    {
+        fprintf(stderr, "Environment variable name is NULL\n");
+        return (NULL);
+    }
+
+    len_var = strlen(var);
+    for (i = 0; environ[i] != NULL; i++)
+    {
+        if (strncmp(environ[i], var, len_var) == 0 && environ[i][len_var] == '=')
+            return (&environ[i][len_var + 1]);
+    }
+    return (NULL);
+}
+
+/**
+ * trim_whitespace - Trims leading and trailing whitespace from a string
+ * @str: The string to trim
+ */
+void trim_whitespace(char *str)
+{
+    int i, j;
+    int len = strlen(str);
+
+    if (str == NULL)
+    {
+        fprintf(stderr, "String to trim is NULL\n");
+        return;
+    }
+
+    i = 0;
+    while (str[i] == ' ' || str[i] == '\t')
+        i++;
+    
+    if (i > 0)
+    {
+        j = 0;
+        while (str[i] != '\0')
+        {
+            str[j] = str[i];
+            i++;
+            j++;
+        }
+        str[j] = '\0';
+    }
+
+    len = strlen(str);
+    i = len - 1;
+    while (i >= 0 && (str[i] == ' ' || str[i] == '\t'))
+    {
+        str[i] = '\0';
+        i--;
+    }
 }
--- main.h
+++ main.h
@@ -2,28 +2,21 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <sys/stat.h>
 
 extern char **environ;
-char *name;
-
-int _strlen(char *string);
-char *_strcat(char *dest, char *src);
-char *_strcpy(char *dest, char *src);
-char *_strdup(char *string);
-int _strcmp(char *s1, char *s2);
-int _str_n_cmp(char *s1, char *s2, int n);
 
 int _printenv(void);
-char *_getenv(char *name);
-
+char *_getenv(char *var);
 char *command_path(char *cmd);
+int command_read(char *s);
+char *find_command_path(const char *cmd);
+int execute(char *cmd_arr[]);
+void trim_whitespace(char *str);
 
-int command_read(char *s, size_t __attribute__((unused)) characters);
-int execute(char *cmd_array[]);
 #endif
